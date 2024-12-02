c9528d6587978492c82938931e07280659bc07ff
M-MubashirIslam <mubashirislam763@gmail.com>
2024-12-03 00:22:38 +0500

    File Updated

--- Source.cpp
+++ Source.cpp
@@ -4,25 +4,9 @@
 #include <string>
 #include <vector>
 #include <variant>
+#include <algorithm>
+#include <list>
 using namespace std;
-
-class match {
-public:
-	int currentPlayerId;
-	int currentTimestamp;
-	vector<int> passIds;
-	int keeperIdA;
-	int keeperIdB;
-	match(int cpi, int ct, vector<int> pi) {
-		currentPlayerId = cpi;
-		currentTimestamp = ct;
-		passIds = pi;
-	}
-	match() {
-		currentPlayerId = -1;
-		currentTimestamp = -1;
-	}
-};
 
 
 class pass {
@@ -31,33 +15,65 @@ public:
 	int time_end;
 	int sender_id;
 	int receiver_id;
-	vector<int> x;
-	vector<int> y;
-	pass(int t_s, int t_e, int s_id, int r_id, vector<int> x, vector<int> y) {
+	pass(int t_s, int t_e, int s_id, int r_id) {
 		time_start = t_s;
 		time_end = t_e;
 		sender_id = s_id;
 		receiver_id = r_id;
-		this->x = x;
-		this->y = y;
 	}
 	void print() {
-		cout << "time_start: " << time_start << endl;
-		cout << "time_end: " << time_end << endl;
-		cout << "sender_id: " << sender_id << endl;
+		cout << "time_start: " << time_start << ", ";
+		cout << "time_end: " << time_end << ", ";
+		cout << "sender_id: " << sender_id << ", ";
 		cout << "receiver_id: " << receiver_id << endl;
-		cout << "x: ";
-		for (int i = 0; i < x.size(); i++) {
-			cout << x[i] << " ";
-		}
-		cout << endl;
-		cout << "y: ";
-		for (int i = 0; i < y.size(); i++) {
-			cout << y[i] << " ";
-		}
-		cout << endl;
 	}
 
+};
+
+class match {
+public:
+	vector<pass> home_team_passes;
+	vector<pass> away_team_passes;
+	vector<int> substituteIds;
+	int last_end_time;
+	bool match_started;
+	match() {
+		last_end_time = -1;
+		match_started = false;
+	}
+
+	void print() {
+		cout << "Home team passes: " << endl;
+		for (int i = 0; i < home_team_passes.size(); i++) {
+			home_team_passes[i].print();
+		}
+		cout << "Away team passes: " << endl;
+		for (int i = 0; i < away_team_passes.size(); i++) {
+			away_team_passes[i].print();
+		}
+	}
+
+	void insertPass(pass p) {
+		if (p.sender_id >= 1 && p.receiver_id <= 14) {
+			home_team_passes.push_back(p);
+		}
+		else if (p.sender_id >= 15 && p.receiver_id <= 28) {
+			away_team_passes.push_back(p);
+		}
+	}
+
+	void setsubstituteIds(vector<int> ids) {
+		substituteIds = ids;
+	}
+
+	bool compare(vector<int> ids) {
+		for (int i = 0; i < ids.size(); i++) {
+			if (substituteIds[i] != ids[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 };
 
 
@@ -66,17 +82,14 @@ class CSVReader {
 public:
 	string fileName;
 	char delimeter;
-	vector<pass> passes;
-	match* matches[14];
-	int currentMatch = 0;
-	CSVReader(string f, char d = ',') {
+	list<match> matches;
+
+	CSVReader(string f) {
 		fileName = f;
-		delimeter = d;
-		for (int i = 0; i < 14; i++){
-			matches[i] = new match();
-		}
+		delimeter = ',';
 		readData();
 	}
+
 	void readData() {
 		ifstream file(fileName);
 		cout << "Reading from the file: " << fileName << endl << endl;
@@ -86,15 +99,14 @@ public:
 		}
 		string line = "";
 
-		//creat all variables
-		int keeperIdsA[14];
-		int keeperIdsB[14];
+		match m;
+		matches.push_back(m);
+
 		int time_start;
 		int time_end;
 		int sender_id;
 		int receiver_id;
-		vector<int> x;
-		vector<int> y;
+
 		getline(file, line);
 		while (getline(file, line)) {
 			vector<string> vec;
@@ -114,83 +126,157 @@ public:
 			sender_id = stoi(vec[2]);
 			receiver_id = stoi(vec[3]);
 			vector<int> x;
-			vector<int> y;
 			for (int i = 4; i < 32; i++) {
-				if (vec[i] != "")
-					x.push_back(stoi(vec[i]));
-				else
-					x.push_back(99999);
+				if (vec[i] == "")
+					x.push_back(i - 3);
 			}
-			for (int i = 32; i < 60; i++) {
-				if (vec[i] != "")
-					y.push_back(stoi(vec[i]));
-				else
-					y.push_back(99999);
-			}
-			passes.push_back(pass(time_start, time_end, sender_id, receiver_id, x, y));
-			int keeperA = 0;
-			int keeperB = 0;
-			int maxpos = 0;
-			int minpos = 0;
-			for (int i = 0; i < x.size(); i++) {
-				if (i == 0) {
-					maxpos = x[i];
-					minpos = x[i];
+			bool insert_success = false;
+			for (auto& m : matches) {
+				if (m.match_started == false) {
+					m.insertPass(pass(time_start, time_end, sender_id, receiver_id));
+					m.setsubstituteIds(x);
+					m.last_end_time = time_end;
+					m.match_started = true;
+					insert_success = true;
+					break;
 				}
-				else {
-					if (x[i] != 99999) {
-						if (x[i] > maxpos) {
-							maxpos = x[i];
-							keeperA = i+1;
-						}
-						if (x[i] < minpos) {
-							minpos = x[i];
-							keeperB = i+1;
-						}
-					}
+				else if (m.last_end_time <= time_start && m.compare(x)) {
+					m.insertPass(pass(time_start, time_end, sender_id, receiver_id));
+					m.last_end_time = time_end;
+					insert_success = true;
+					break;
 				}
 			}
-			cout << maxpos << " " << minpos << endl;
-			cout << keeperA << " " << keeperB << endl;
-
-			/*vector<int> pi;
-			pi.push_back(passes.size() - 1);
-			if (currentMatch == 0) {
-				matches[0] = new match(receiver_id, time_end, pi);
-				matches[0]->keeperIdA = keeperA;
-				matches[0]->keeperIdB = keeperB;
-				currentMatch++;
+			if (insert_success == false) {
+				match m;
+				m.insertPass(pass(time_start, time_end, sender_id, receiver_id));
+				m.setsubstituteIds(x);
+				m.last_end_time = time_end;
+				m.match_started = true;
+				matches.push_back(m);
 			}
-			else {
-				for (int i = 0; i < currentMatch; i++) {
-					if (i < 14 && matches[i] != nullptr){
-						if (matches[i]->currentPlayerId == sender_id && matches[i]->currentTimestamp <= time_start && (keeperA == matches[i]->keeperIdA || keeperB == matches[i]->keeperIdB)) {
-							matches[i]->passIds.push_back(passes.size() - 1);
-							matches[i]->currentTimestamp = time_end;
-							matches[i]->currentPlayerId = receiver_id;
-							break;
-						}
-						else if (i == currentMatch - 1) {
-							matches[i] = new match(receiver_id, time_end, pi);
-							matches[i]->keeperIdA = keeperA;
-							matches[i]->keeperIdB = keeperB;
-							currentMatch++;
-							break;
-						}
-					}
-				}
-
-			}
-
 		}
-		cout << "Number of matches identified: ";
-		cout << currentMatch << endl;*/
 		file.close();
 	}
+
+	void printMatchesDataSet() {
+		int i = 1;
+		for (auto& m : matches) {
+			cout << "Match " << i << endl;
+			m.print();
+			i++;
+			cout << endl << endl;
+			char ch;
+			cin >> ch;
+		}
+	}
+
+	void generateSnapShots(int seconds, match m) {
+		//adjacency matrix
+		int snap_shot_graph[14][14] = { 0 };
+		int reciever_id;
+		int sender_id;
+		int time_past = 0;
+		int snap_shot_number = 1;
+		for (int i = 0; i < m.home_team_passes.size(); i++) {
+			reciever_id = m.home_team_passes[i].receiver_id;
+			sender_id = m.home_team_passes[i].sender_id;
+			time_past += m.home_team_passes[i].time_end;
+			if (time_past <= seconds) {
+				snap_shot_graph[sender_id - 1][reciever_id - 1]++;
+			}
+			else {
+				string fileName = "home_team_snapshot.txt_" + to_string(snap_shot_number) + ".txt";
+				generateGraphProfile(snap_shot_graph, fileName);
+				intializeArray(snap_shot_graph);
+				snap_shot_graph[sender_id - 1][reciever_id - 1]++;
+				time_past += m.home_team_passes[i].time_end;
+				snap_shot_number++;
+			}
+		}
+		snap_shot_number = 1;
+		for(int i = 0; i < m.away_team_passes.size(); i++) {
+			reciever_id = m.away_team_passes[i].receiver_id;
+			sender_id = m.away_team_passes[i].sender_id;
+			time_past += m.away_team_passes[i].time_end;
+			if (time_past <= seconds) {
+				snap_shot_graph[sender_id - 15][reciever_id - 15]++;
+			}
+			else {
+				string fileName = "away_team_snapshot_" + to_string(snap_shot_number) + ".txt";
+				generateGraphProfile(snap_shot_graph, fileName);
+				intializeArray(snap_shot_graph);
+				snap_shot_graph[sender_id - 15][reciever_id - 15]++;
+				time_past += m.away_team_passes[i].time_end;
+				snap_shot_number++;
+			}
+		}
+	}
+
+	void generateGraphProfile(int arr[14][14], string fileName) {
+		ofstream file(fileName);
+		string comment = "# number of Players\n";
+		file << comment;
+		file << "14\n";
+		comment = "# Player Attributes\n";
+		file << comment;
+		for (int i = 0; i < 14; i++) {
+			file << i << " ";
+			comment = "Player " + to_string(i + 1) + "\n";
+			file << comment;
+		}
+		for (int i = 0; i < 14; ++i) {
+			comment = "# Pases By player " + to_string(i) + "\n";
+			file << comment;
+			int edges = 0;
+			for (int j = 0; j < 14; j++) {
+				if (arr[i][j] != 0) {
+					edges++;
+				}
+			}
+			file << edges << endl;
+			if (edges != 0) {
+				for (int j = 0; j < 14; j++) {
+					if (arr[i][j] != 0) {
+						file << i << " " << j << " " << arr[i][j] << endl;
+					}
+				}
+			}
+		}
+		file.close();
+		return;
+	}
+
+	void generateSnapShots(int seconds, int matchNumber) {
+		int i = 1;
+		for (auto& m : matches) {
+			if (i == matchNumber) {
+				generateSnapShots(seconds, m);
+				break;
+			}
+			i++;
+		}
+	}
+
+	void intializeArray(int arr[14][14]) {
+		for (int i = 0; i < 14; i++) {
+			for (int j = 0; j < 14; j++) {
+				arr[i][j] = 0;
+			}
+		}
+	}
+
 };
 
-
 int main() {
-	CSVReader reader("first200.csv");
+	CSVReader reader("passes_in_EXCEL_FORMAT.csv");
+	reader.printMatchesDataSet();
+	/*int matchNumber, timeWindow;
+	cout<< "Enter the match number to genrate snapshots: ";
+	cin >> matchNumber;
+	cout<< "Enter the time window in milliseconds: ";
+	cin >> timeWindow;*/
+	reader.generateSnapShots(300000, 2);//timeWindow, matchNumber);
+	cout << "Program End!!!";
 	return 0;
 }
